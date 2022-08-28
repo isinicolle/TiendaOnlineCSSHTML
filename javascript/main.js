3227509d158b86cb06b2fbf738eb7c5b0f2b1cfc
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-28 16:30:05 -0600

    actualizacion css
    actualizacion css

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,11 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const productdetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+productdetail.addEventListener('click', toggle);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
